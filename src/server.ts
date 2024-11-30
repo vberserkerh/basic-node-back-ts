2669788f78e7e13e46ee8c0b5a44eb61df9d0d6a
Hugodk33 <hugodk33@gmail.com>
2024-11-30 11:28:33 -0300

    second commit

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,23 @@ import userRoutes from "./infrastructure/web/routes/user.routes";
 
 dotenv.config();
 
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+// Configuração do CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // Permitir o frontend acessar
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true // Habilita envio de cookies e headers como Authorization
+}));
+
 app.use(express.json());
 app.use("/api", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
